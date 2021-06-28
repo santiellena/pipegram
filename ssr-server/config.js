4e5d39910bebf325f9e35cb712f667e7b6a74890
ESanti <santiellenacm@gmail.com>
2021-06-28 16:05:49 -0300

    buena parte de la implementacion de la instalacion del commit anterior

--- ssr-server/config.js
+++ ssr-server/config.js
@@ -4,6 +4,10 @@ const configs = {
     apiUrl: process.env.API_URL || 'http://localhost:3000/',
     apiKeyToken: process.env.API_KEY_TOKEN || 'a6586ab75deabcd34a082f597bfa1b09a218720dd8af6a3ffded809237bfb08e',
     mode: process.env.SSR_MODE || 'dev',
+    oauth: {
+        googleIdClient: '142666905100-hj4qtp7vb0nm3qb1n4nqkp7136mdmlkt.apps.googleusercontent.com',
+        googleClientSecret: 'G-mg6-aqqfrC9ALqJqGnq5EP',
+    },
 };
 
 module.exports = configs;
